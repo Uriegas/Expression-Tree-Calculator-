38933970bfc127aa2163812ac2ac8c7d9d6d0f3b
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-05-07 13:07:02 -0500

    Terms datatype(class)

---
+++ src/main/java/com/spolancom/Terms.java
@@ -0,0 +1,21 @@
+package com.spolancom;
+
+public class Terms {
+    /**
+     * Number or result of operation
+     */
+    private float value;
+    /**
+     * Operator(+,-,/,*,sin,cos,tan)
+     * This could be a char if trig funcs are converted as chars
+     */
+    private String operator;
+    /**
+     * It is a number
+     */
+    public Terms(float value){this.value = value;}
+    /**
+     * It is an operator
+     */
+    public Terms(String operator){this.operator = operator;}
+}

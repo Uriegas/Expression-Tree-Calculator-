1f8859afdd986ccea89e48f8ac7f23c36795bebf
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-05-11 17:09:53 -0500

    Trash code removed

--- src/main/java/com/spolancom/Calculator.java
+++ src/main/java/com/spolancom/Calculator.java
@@ -34,105 +34,7 @@ public class Calculator{
         return result;
     }
 
-    private boolean isNumber(char c){
-        if( c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
-            c == '6' || c == '7' || c == '8' || c == '9' || c == '.' )
-            return true;
-        else
-            return false;
-    }
-    private boolean isOperand(char c){
-        if( c == '+' || c == '-' || c == '*' || c == '/' ||
-            c == '^' || c == '!' || c == '@' || c == '#' || c == '$' )
-            return true;
-        else
-            return false;
-    }
     
-    /**
-     * Tokenizer
-     * Converts string to an array of tokens
-     */
-    /*
-    public ArrayList<Token> Tokenizer(String s){
-        ArrayList<Token> tokens = new ArrayList<>();
-        String number = "";*/
-        /**
-         * Replace functions names for char
-         *//*
-        s = s.toLowerCase();
-        s = s.replaceAll(" ","");
-        s = s.replaceAll("pow", "^");
-        s = s.replaceAll("sin", "!");
-        s = s.replaceAll("cos", "@");
-        s = s.replaceAll("tan", "#");
-        s = s.replaceAll("sqrt", "$");
-*/
-        /**
-         * Algorithm
-         *//*
-        boolean func_par_flag = false;
-
-        for( int i = 0; i < s.length(); i++ ){
-            //Is an operator
-            if(s.charAt(i) == '+')
-                tokens.add(new Operator(Operator_Type.ADD));
-            else if(s.charAt(i) == '-')
-                tokens.add(new Operator(Operator_Type.SUB));
-            else if(s.charAt(i) == '*')
-                tokens.add(new Operator(Operator_Type.MUL));
-            else if(s.charAt(i) == '/')
-                tokens.add(new Operator(Operator_Type.DIV));
-            else if(s.charAt(i) == '^')
-                tokens.add(new Operator(Operator_Type.POW));
-            else if(s.charAt(i) == '!')
-                tokens.add(new Operator(Operator_Type.SIN));
-            else if(s.charAt(i) == '@')
-                tokens.add(new Operator(Operator_Type.COS));
-            else if(s.charAt(i) == '#')
-                tokens.add(new Operator(Operator_Type.TAN));
-            else if(s.charAt(i) == '$')
-                tokens.add(new Operator(Operator_Type.SQRT));
-            else if(s.charAt(i) == '('){
-                if(tokens.get(tokens.size()-1).op_type){//If its an operator so a func parent
-                    func_par_flag = true;
-                    if(tokens.get(tokens.size()-1).isFunction())
-                        tokens.add(new Agrupator(Agrupator_Type.FUNC_LEFT));
-                    else
-                        tokens.add(new Agrupator(Agrupator_Type.ASSOCIATIVE_LEFT));
-                }
-            }
-            else if(s.charAt(i) == ')'){
-                func_par_flag = false;
-                if(func_par_flag){//If we have a func par in stack
-                    tokens.add(new Agrupator(Agrupator_Type.FUNC_RIGTH));
-                }
-                else{
-                    tokens.add(new Agrupator(Agrupator_Type.ASSOCIATIVE_RIGTH));
-                }
-            }
-            else if(s.charAt(i) == '[')
-                tokens.add(new Agrupator(Agrupator_Type.ASSOCIATIVE_LEFT));
-            else if(s.charAt(i) == ']')
-                tokens.add(new Agrupator(Agrupator_Type.ASSOCIATIVE_RIGTH));
-            else if(this.isNumber(s.charAt(i))){//Is an operand aka number
-                number += s.charAt(i);
-                try{
-                    if(!this.isNumber(s.charAt(i+1))){
-                        tokens.add(new Operand(Operand_Type.NUMBER, Double.parseDouble(number)));
-                        number = "";
-                    }
-                }catch(Exception e){//End of array and we got a number
-                        tokens.add(new Operand(Operand_Type.NUMBER, Double.parseDouble(number)));
-                }
-            }
-            else if(s.charAt(i) == 'x')
-                tokens.add(new Operand(Operand_Type.VARIABLE));
-        }
-        
-        return tokens;
-    }*/
-
     /**
      * Tokenizer
      * @param s
@@ -160,6 +62,7 @@ public class Calculator{
         }
         return out; 
     }
+
     /**
      * toRPN function
      * Converts an array of tokens to a stack in Reverse Polish Notation
